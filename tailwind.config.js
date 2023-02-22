13b0f9b9fa6bbec846f3d47849a0ee5e63f626d5
Lexzee <developerlexzee@gmail.com>
2023-02-23 00:34:17 +0100

    re-initialized tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    screen: {
+      sm: '375px',
+      md: '728px',
+      lg: '1440px',
+    },
+    extend: {
+      colors: {
+        almostWhite: 'hsl(0, 0%, 98%)',
+        mediumGray: 'hsl(0, 0%, 41%)',
+        almostBlack: 'hsl(0, 0%, 8%)'
+      },
+      fontFamily: {
+        'epilogue': ['Epilogue', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
